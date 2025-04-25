d916c06fda2daf4e394dd83df9c364a4b4b6c709
Franco Olivero <132franco@gmail.com>
2025-04-25 20:21:25 -0300

    feat(user): add validation decorators to CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsNumber, IsPositive, IsString, MinLength, IsEmail } from "class-validator";
+
+
 export class CreateUserDto {
+
+  @IsString()
   name: string
+
+  @IsEmail()
   email: string
 }
